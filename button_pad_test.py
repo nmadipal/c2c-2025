a6e6d6c3605990bade00d62f3d0f20150d259cf3
harley-cumming <harley.cumming@cirrus.com>
2025-07-02 15:51:52 -0500

    Removed commented out code

--- button_pad_test.py
+++ button_pad_test.py
@@ -16,19 +16,10 @@ while(True):
             blu_led = pad.blu_list[row]
             grn_led = pad.grn_list[row]
             led = blu_led
-            # led_list = [red_led, blu_led, grn_led]
             led_col = pad.led_col_list[col]
-            if button_state[col][row]:
-            #     # If the same button is being pressed, light up the same LED as last time
-            #     if last_button_state[col][row] == button_state[col][row]:
-            #         led = led_list[last_led_idx[col][row]]
-            #     else:
-            #         led_idx = random.randint(0, 2)
-            #         led = led_list[led_idx]
-            #         last_led_idx[col][row] = led_idx                    
+            if button_state[col][row]:            
                 pad.set_led(led, led_col, True)
             else:
-                # led = led_list[last_led_idx[col][row]]
                 pad.set_led(led, led_col, False)
 
                 
